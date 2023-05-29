3e683433662e6605c94a86a0ed5a1afe1e4357f6
alivi <hakeemalivi5@gmail.com>
2023-05-29 19:55:41 +0500

    react hooks

--- src/App.js
+++ src/App.js
@@ -1,23 +1,22 @@
-import logo from './logo.svg';
 import './App.css';
-
+import "../node_modules/bootstrap/dist/css/bootstrap.min.css";
+import React, {useState} from "react";  
 function App() {
+  const fruits  = ["apple","mango","pineapple","avocado","strawberry","watermelon"];
+  const [data, setData] = new useState(fruits);
+  function handler(event){
+    const search = event.target.value;
+    const filtered = fruits.filter((data) => data.includes(search));  
+    setData(filtered);
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <label > Search</label>
+      <input type='text' id='search' placeholder='Search' onChange={handler}></input><br/>
+      <ul className='list-group'>
+        {data.map((item) => <li className='list-group-item'>{item}</li>)}
+      </ul>
     </div>
   );
 }
